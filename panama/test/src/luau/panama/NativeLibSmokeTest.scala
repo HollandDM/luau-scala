491278432e09fc829227080c223e36bb2d0dc94a
HoangDinh <hoangdinh@anduintransact.com>
2026-06-09 11:18:34 +0700

    P01 Tasks 5-11: shim build tracking, smoke tests, CI, gitignore

---
+++ panama/test/src/luau/panama/NativeLibSmokeTest.scala
@@ -0,0 +1,9 @@
+package luau.panama
+
+import munit.FunSuite
+
+class NativeLibSmokeTest extends FunSuite {
+  test("placeholder - native library smoke test".ignore) {
+    // P04 will implement the full test
+  }
+}
---
+++ wasm/test/src/luau/wasm/WasmModuleSmokeTest.scala
@@ -0,0 +1,9 @@
+package luau.wasm
+
+import munit.FunSuite
+
+class WasmModuleSmokeTest extends FunSuite {
+  test("WASM module loads and lx_version returns 1".ignore) {
+    // P05 will implement the full test
+  }
+}
